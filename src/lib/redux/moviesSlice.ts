d9775f9dabb299d636c58e85422f1f0dc443d8a4
elizaveetaPopova <eli.bychkova@gmail.com>
2025-07-29 11:51:26 +0300

    installed dependencies and create store

---
+++ src/lib/redux/moviesSlice.ts
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const movieSlice = createSlice({
+  name: 'movies',
+  initialState: {
+    movies: [],
+  },
+  reducers: {
+    addMovies: (state, action) => {
+      state.movies = action.payload;
+    },
+  },
+});
+
+export const { addMovies } = movieSlice.actions;
+export default movieSlice.reducer;
